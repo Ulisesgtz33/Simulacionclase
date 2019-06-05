ab55fdcefd3738a1cc98adb7994f5275645150da
Ulisesgtz33 <51324868+Ulisesgtz33@users.noreply.github.com>
2019-06-05 08:47:10 -0500

    Add files via upload

---
+++ ejercciosclase.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[25]:
+
+
+#ejercicio1
+numerosdivisibles7=[]
+for i in range(1000):
+    i=i+1
+    if (i%7)==0:
+        
+        numerosdivisibles7.append(int (i))
+print ("la lista es:", numerosdivisibles7)
+
+
+# In[34]:
+
+
+#ejercicio2
+
+x=[str(i) for i in range (1000)]
+for j in range(1000):
+    if x[j]==:
+    
+
+        
+
